abd96bb6269813705a978cd2c9200cf4bcc332e5
hoang huy <shacohuy03@gmail.com>
2023-05-26 19:10:58 +0700

    update cart

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -7,14 +7,26 @@ const wrapSlide=document.querySelector('.header__slider');
 const btnNext=document.querySelector('.header__slider-control-right');
 const btnPre=document.querySelector('.header__slider-control-left');
 
+var ttl;
+
 btnNext.addEventListener('click', function(e){
     const list=document.querySelectorAll('.header__slider-item');
     //list[0,1,2]
+    ttl=0;
     wrapSlide.append(list[0]);
 })
 
 btnPre.addEventListener('click', function(e){
     const list=document.querySelectorAll('.header__slider-item');
     //list[0,1,2]
+    ttl=0;
     wrapSlide.prepend(list[list.length-1]);
 })
+
+setInterval (function(){
+    ttl++;
+    if(ttl>3){
+        btnNext.click(); 
+    }
+    
+}, 4000);
